eb3d0300d1473d1c753b49b00f5400b1a77bd50f
GarethMalekaMotloutsi <Gareth.motloutsi@gmail.com>
2026-07-13 22:20:05 +0200

    Fetch and display user data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => {
+      setUsers(data)
+    })
+}, [])
+
   return (
     <div className="app">
       <h1>User Directory</h1>
-      <p>Loading users...</p>
+
+    <div className="user-list">
+    {users.map((user) => (
+    <div className="user-card" key={user.id}>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      <p>Company: {user.company.name}</p>
+    </div>
+  ))}
+</div>
     </div>
   )
 }
