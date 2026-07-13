9d7e7a744345a2f43563bce4d8da83d7ad847c11
GarethMalekaMotloutsi <Gareth.motloutsi@gmail.com>
2026-07-13 21:56:56 +0200

    Set up React project and create initial app layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>User Directory</h1>
+      <p>Loading users...</p>
+    </div>
+  )
+}
+
+export default App
